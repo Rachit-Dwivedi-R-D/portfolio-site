6f3c2d7c8b68442fd72d42f5ac54acb7e7afc621
Rachit-Dwivedi-R-D <rachit0dwivedi@gmail.com>
2025-05-20 08:24:47 +0530

    Updated Metadata and Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,16 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
 const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Rachit Dwivedi | Game Developer',
   description: 'Portfolio of Rachit Dwivedi — Game Developer, Designer & Animator',
   openGraph: {
@@ -22,7 +22,7 @@ export const metadata = {
     siteName: 'Rachit Dwivedi Portfolio',
     images: [
       {
-        url: '/preview-image.png', // Add a suitable image in your public/ folder
+        url: '/preview-image.png', // Add this image in your public folder
         width: 1200,
         height: 630,
         alt: 'Rachit Dwivedi Portfolio Preview',
@@ -38,15 +38,16 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
 }
